8c8f5f17055709576c2b0fb0cb192d7dc4301b1f
cokeefe <chaemobile@gmail.com>
2013-12-05 17:24:37 -0500

    add #add test case

--- tdd-kata-1_spec.rb
+++ tdd-kata-1_spec.rb
@@ -1,5 +1,21 @@
 require_relative 'tdd-kata-1'
 
 describe Calculator do
+  let(:calculator) { Calculator.new }
+
+  context "#add" do
+    it "should only accept strings" do
+    end
+
+    it "should return 0, given 0 numbers" do
+    end
+
+    it "should return the number, given 1 number" do
+    end
+
+    it "should return the sum, given 2 numbers" do
+    end
+  end
+
 end
 
