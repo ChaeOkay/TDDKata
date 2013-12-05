757054ee02d15520c5dbf7c5509867738b415cc4
cokeefe <chaemobile@gmail.com>
2013-12-05 17:32:01 -0500

    calculator invalid parameters

--- tdd-kata-1.rb
+++ tdd-kata-1.rb
@@ -1,2 +1,8 @@
 class Calculator
+
+  attr_accessor :numbers
+  def initialize(numbers)
+    raise ArgumentError, 'Argument is not a string' unless numbers.is_a? String
+    @numbers = numbers
+  end
 end
--- tdd-kata-1_spec.rb
+++ tdd-kata-1_spec.rb
@@ -1,19 +1,24 @@
 require_relative 'tdd-kata-1'
 
 describe Calculator do
-  let(:calculator) { Calculator.new }
+
+  context 'invalid params' do
+    it "should raise an error if param is not a string" do
+      expect{ Calculator.new(1)}.to raise_error ArgumentError
+    end
+  end
 
   context "#add" do
-    it "should only accept strings" do
-    end
-
     it "should return 0, given 0 numbers" do
+      pending
     end
 
     it "should return the number, given 1 number" do
+      pending
     end
 
     it "should return the sum, given 2 numbers" do
+      pending
     end
   end
 
