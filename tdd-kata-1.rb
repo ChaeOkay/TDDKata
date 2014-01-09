c45f11ab9e34b891cda5c3c8a6a52b8937735aca
cokeefe <chaemobile@gmail.com>
2014-01-09 07:18:40 -0500

    chpt4 add public and private interfaces, implement hash initialization params

--- tdd-kata-1.rb
+++ tdd-kata-1.rb
@@ -1,19 +1,23 @@
 class NumberExtractor
   attr_reader :delimeter, :string_of_numbers, :numbers
 
-  def initialize(text)
+  def initialize(args)
+    text = args[:string_of_numbers]
     raise ArgumentError, 'Argument is not a string' unless text.is_a? String
     format(text)
   end
+
+  def numbers
+    @numberes = split_numbers.map! { |number| number.to_i }
+  end
+
+
+  private
 
   def format(text)
     new_text = text.match(/\/{2}(?<delimeter>.)\n(?<numbers>.*)/) || {}
     @string_of_numbers = new_text[:numbers] || format_newline(text)
     @delimeter = new_text[:delimeter] || ','
-  end
-
-  def numbers
-    @numberes = split_numbers.map! { |number| number.to_i }
   end
 
   def format_newline(text)
@@ -29,14 +33,17 @@ end
 class Calculator
   attr_reader :numbers
 
-  def initialize(numbers)
-    @numbers = numbers  #numbers is an array of integers
+  def initialize(args)
+    @numbers = args[:numbers]  #numbers is an array of integers
     check_errors
   end
 
   def add
     display numbers.inject(:+)
   end
+
+
+  private
 
   def display(result)
     result.nil? ? 0 : result
--- tdd-kata-1_spec.rb
+++ tdd-kata-1_spec.rb
@@ -3,23 +3,23 @@ require_relative 'tdd-kata-1'
 describe NumberExtractor do
   context 'invalid params' do
     it 'should raise an error if param is not a string' do
-      expect{ NumberExtractor.new(1)}.to raise_error ArgumentError
+      expect{ NumberExtractor.new({string_of_numbers: 1}) }.to raise_error ArgumentError
     end
   end
 
   context 'converting string to integers' do
     it 'should convert a string into an array of numbers' do
-      extractor = NumberExtractor.new("1,2,3")
+      extractor = NumberExtractor.new({string_of_numbers: "1,2,3"})
       expect(extractor.numbers).to eq [1,2,3]
     end
 
     it 'should accept /n as split points' do
-      extractor = NumberExtractor.new("1/n3,4")
+      extractor = NumberExtractor.new({string_of_numbers: "1/n3,4"})
       expect(extractor.numbers).to eq [1,3,4]
     end
 
     it 'should detect a new delimeter' do
-      extractor = NumberExtractor.new("//;\n5;3;9")
+      extractor = NumberExtractor.new({string_of_numbers: "//;\n5;3;9"})
       expect(extractor.numbers).to eq [5,3,9]
     end
   end
@@ -28,25 +28,25 @@ end
 describe Calculator do
   context 'positive numbers' do
     before :each do
-      @calculator = Calculator.new(extractor.numbers)
+      @calculator = Calculator.new({numbers: extractor.numbers})
     end
 
     context '0 integers' do
-      let(:extractor) { NumberExtractor.new("") }
+      let(:extractor) { NumberExtractor.new({string_of_numbers: ""}) }
       it 'should return 0' do
         expect(@calculator.add).to eq 0
       end
     end
 
     context '1 integer' do
-      let(:extractor) { NumberExtractor.new("1") }
+      let(:extractor) { NumberExtractor.new({string_of_numbers: "1"}) }
       it 'should return 1' do
         expect(@calculator.add).to eq 1
       end
     end
 
     context '2 integers' do
-      let(:extractor) { NumberExtractor.new("25,5") }
+      let(:extractor) { NumberExtractor.new({string_of_numbers: "25,5"}) }
       it 'should return 30' do
         expect(@calculator.add).to eq 30
       end
@@ -56,7 +56,7 @@ describe Calculator do
   context 'negative numbers' do
     it 'should raise an error' do
       expect{
-        extractor = NumberExtractor.new("10,-9,-8,7")
+        extractor = NumberExtractor.new({string_of_numbers: "10,-9,-8,7"})
         calculator = Calculator.new(extractor.numbers)
       }.to raise_error
     end
